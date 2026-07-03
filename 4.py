7b267ce83d9c5434c31d2ee59955c20e756f17ff
Rachitthakurr <ranarachit70@gmail.com>
2026-07-03 09:46:08 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,8 @@
+length = float(input("enter the length: "))
+width = float(input("enter the width: "))
+
+area = length * width
+
+# area = int(length) * int(width)
+
+print(f"the area is {area} cm²")
