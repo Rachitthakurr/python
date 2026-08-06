3f6c5912423aadcf081645af86f7e2ce8d52bd5a
Rachitthakurr <ranarachit70@gmail.com>
2026-08-06 22:09:29 +0530

    class

---
+++ apnaclg/8.py
@@ -0,0 +1,11 @@
+str1 = "rachit"
+len1 = len(str1)
+print("length of string is : ", len1)
+
+str2 = "Thakur"
+len2 = len(str2)
+print("length of string is : ", len2)
+
+final_str = str1 + " " + str2
+print("final string is : ", final_str)
+print("length of final string is : ", len(final_str))
---
+++ apnaclg/class.py
@@ -0,0 +1,41 @@
+# class Student:
+#     def __init__(self, phy, chem, bio):
+#         self.phy = phy
+#         self.chem = chem
+#         self.bio = bio 
+
+
+    # def calcculate_percentage(self):
+    #     self.percentage = str((self.phy + self.chem + self.bio) / 3) + "%"
+
+
+    # @property
+    # def percentage(self):
+    #     return str((self.phy + self.chem + self.bio) / 3) + "%"
+
+        
+# stu1 = Student(98, 97, 99)
+# print(stu1.percentage)
+
+# stu1.phy = 86
+# print(stu1.percentage)
+
+
+#POLYMORPHISM IN PYTHON
+
+# int = "polymorphism in python"
+# def upper(string):
+#     return string.upper()
+
+# upper = upper(int)
+# print(upper)
+
+
+# OPERATOR OVERLOADING IN PYTHON
+print(1 + 2) #3 addition
+print(type(1))
+
+print("rachit" + "Thakur") #concatenate
+print(type("rachit"))
+print([1,2,3] + [4,5,6]) #merge two lists
+print(type([1,2,3]))
